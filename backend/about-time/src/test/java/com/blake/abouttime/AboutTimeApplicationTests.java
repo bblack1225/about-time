7991424d625117fede80b5001925ed777d52c65d
bblack1225 <black60137@gmail.com>
2021-07-27 12:46:49 +0800

    init backend project

---
+++ backend/about-time/src/test/java/com/blake/abouttime/AboutTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blake.abouttime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
